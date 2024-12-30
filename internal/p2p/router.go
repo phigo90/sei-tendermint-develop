f3ef885ce004d5a325b876a84ec0866dfd5ffdcd
DefiPhigo <philipp.golda@googlemail.com>
2024-12-30 23:39:46 +0100

    Add Info Output to router.go

--- internal/p2p/router.go
+++ internal/p2p/router.go
@@ -648,7 +648,7 @@ func (r *Router) connectPeer(ctx context.Context, address NodeAddress) {
 		}
 		return
 	}
-	r.logger.Error("Guten Rutsch Jungs")
+	r.logger.Info("Guten Rutsch Jungs")
 	peerInfo, err := r.handshakePeer(ctx, conn, address.NodeID)
 	switch {
 	case errors.Is(err, context.Canceled):
