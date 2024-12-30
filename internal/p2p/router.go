50806cc59bfa8c78666de6152d5832dca65cb430
DefiPhigo <philipp.golda@googlemail.com>
2024-12-30 21:06:33 +0100

    Add Info Output to router.go

--- internal/p2p/router.go
+++ internal/p2p/router.go
@@ -539,6 +539,7 @@ func (r *Router) openConnection(ctx context.Context, conn Connection) {
 		return
 	}
 
+	r.logger.Info("Hallo Eric")
 	// FIXME: The peer manager may reject the peer during Accepted()
 	// after we've handshaked with the peer (to find out which peer it
 	// is). However, because the handshake has no ack, the remote peer
